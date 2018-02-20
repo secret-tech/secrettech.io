e6726302796204877f4a5ecf43bac5b5b9a190d6
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-02-21 02:58:36 +0800

    Add favicons

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,23 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('gulp-cssnano');
 const gcmq = require('gulp-group-css-media-queries');
 const browserSync = require('browser-sync');
+const realFavicon = require('gulp-real-favicon');
+const fs = require('fs');
 
 // paths
 
 const src = {
   html: 'src/pug/*.pug',
   css: 'src/css/styles.css',
-  stls: 'src/css/**/*.css'
+  stls: 'src/css/**/*.css',
+  favicon: 'src/images/favicon.png'
 };
 
 const dist = {
   all: 'build/**/*',
   html: 'build/',
-  css: 'build/css/'
+  css: 'build/css/',
+  favicon: 'build/icons'
 };
 
 // rules
@@ -67,6 +71,89 @@ gulp.task('browser-sync', () => {
 
 gulp.task('build', () => {
   gulp.start('html', 'css');
+});
+
+const FAVICON_DATA_FILE = 'faviconData.json';
+
+gulp.task('generate-favicon', (done) => {
+	realFavicon.generateFavicon({
+		masterPicture: src.favicon,
+		dest: dist.favicon,
+		iconsPath: './icons/',
+		design: {
+			ios: {
+				pictureAspect: 'backgroundAndMargin',
+				backgroundColor: '#ffffff',
+				margin: '14%',
+				assets: {
+					ios6AndPriorIcons: false,
+					ios7AndLaterIcons: false,
+					precomposedIcons: false,
+					declareOnlyDefaultIcon: true
+				}
+			},
+			desktopBrowser: {},
+			windows: {
+				pictureAspect: 'noChange',
+				backgroundColor: '#ffc40d',
+				onConflict: 'override',
+				assets: {
+					windows80Ie10Tile: false,
+					windows10Ie11EdgeTiles: {
+						small: false,
+						medium: true,
+						big: false,
+						rectangle: false
+					}
+				}
+			},
+			androidChrome: {
+				pictureAspect: 'backgroundAndMargin',
+				margin: '17%',
+				backgroundColor: '#ffffff',
+				themeColor: '#ffffff',
+				manifest: {
+					display: 'standalone',
+					orientation: 'notSet',
+					onConflict: 'override',
+					declared: true
+				},
+				assets: {
+					legacyIcon: false,
+					lowResolutionIcons: false
+				}
+			},
+			safariPinnedTab: {
+				pictureAspect: 'blackAndWhite',
+				threshold: 50,
+				themeColor: '#333333'
+			}
+		},
+		settings: {
+			scalingAlgorithm: 'Mitchell',
+			errorOnImageTooSmall: false,
+			readmeFile: false,
+			htmlCodeFile: false,
+			usePathAsIs: false
+		},
+		markupFile: FAVICON_DATA_FILE
+	}, () => {
+		done();
+	});
+});
+
+gulp.task('inject-favicon-markups', () =>
+  gulp.src(['build/*.html'])
+		.pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
+		.pipe(gulp.dest(dist.html)));
+
+gulp.task('check-for-favicon-update', (done) => {
+	const currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
+	realFavicon.checkForUpdates(currentVersion, function(err) {
+		if (err) {
+			throw err;
+		}
+	});
 });
 
 gulp.task('deploy',
